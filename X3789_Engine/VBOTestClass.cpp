34479e882d139f6b9c619cf5228dcd7cf6becaa9
Czerniecki Wojciech <czerniecki.wojciech@gmail.com>
2014-09-19 10:15:08 +0200

    working VBO test class :D

--- X3789_Engine/VBOTestClass.cpp
+++ X3789_Engine/VBOTestClass.cpp
@@ -35,6 +35,7 @@ void VBOTestClass::PreDraw()
 		0.0f, 1.0f, 0.0f,
 		0.0f, 0.0f, 1.0f,
 		1.0f, 1.0f, 0.0f,
+
 		1.0f, 0.0f, 1.0f,
 		0.0f, 1.0f, 1.0f,
 		1.0f, 1.0f, 1.0f,
@@ -49,8 +50,8 @@ void VBOTestClass::PreDraw()
 		0, 1, 5,
 		0, 5, 4,
 
-		0, 3, 4,
-		0, 4, 7,
+		3, 4, 0,
+		3, 4, 7,
 
 		2, 3, 7,
 		2, 6, 7,
@@ -65,21 +66,21 @@ void VBOTestClass::PreDraw()
 	//vertices
 	glGenBuffers(1, &vertex_buffer_id);	GL_ERROR();
 	glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_id);	GL_ERROR();
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices) * sizeof(GLfloat), 0, GL_STATIC_DRAW);	GL_ERROR();
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices) * sizeof(GLfloat), vertices, GL_STATIC_DRAW);	GL_ERROR();
 
-	GLvoid* vbo_buffer = glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
+	/*GLvoid* vbo_buffer = glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
 
 	memcpy(vbo_buffer, vertices, sizeof(vertices) * sizeof(GLfloat));
 
-	glUnmapBuffer(GL_ARRAY_BUFFER);
+	glUnmapBuffer(GL_ARRAY_BUFFER);*/
 
-	glVertexPointer(3, GL_FLOAT, 0, (GLvoid*)0);
+	//glVertexPointer(3, GL_FLOAT, 0, (GLvoid*)0);
 
-	/*colors
+	//colors
 	glGenBuffers(1, &color_buffer_id);	GL_ERROR();
 	glBindBuffer(GL_ARRAY_BUFFER, color_buffer_id);	GL_ERROR();
-	glBufferData(GL_ARRAY_BUFFER, sizeof(colors) * sizeof(float), colors, GL_STATIC_DRAW);	GL_ERROR();
-	*/
+	glBufferData(GL_ARRAY_BUFFER, sizeof(colors) * sizeof(GLfloat), colors, GL_STATIC_DRAW);	GL_ERROR();
+	
 	//indices
 	glGenBuffers(1, &indices_buffer_id);	GL_ERROR();
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indices_buffer_id);	GL_ERROR();
@@ -98,15 +99,16 @@ void VBOTestClass::Draw(GLuint shader)
 
 	glEnableVertexAttribArray(id);
 	glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_id); GL_ERROR();
-	glVertexAttribPointer(id, 3, GL_FLOAT, GL_FALSE, sizeof(float), (void*)0); GL_ERROR();
+	glVertexAttribPointer(id, 3, GL_FLOAT, GL_FALSE, 0, (void*)0); GL_ERROR();
 
-	//id = glGetAttribLocationARB(shader, "inColor"); GL_ERROR();
-	//glBindBufferARB(GL_ARRAY_BUFFER_ARB, color_buffer_id); GL_ERROR();
-	//glVertexAttribPointerARB(id, 3, GL_FLOAT, GL_FALSE, GL_FALSE, (void*)0); GL_ERROR();
+	id = glGetAttribLocationARB(shader, "inColor"); GL_ERROR();
+	glEnableVertexAttribArray(id);
+	glBindBufferARB(GL_ARRAY_BUFFER_ARB, color_buffer_id); GL_ERROR();
+	glVertexAttribPointerARB(id, 3, GL_FLOAT, GL_FALSE, 0, (void*)0); GL_ERROR();
 
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indices_buffer_id); GL_ERROR();
 
-	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, (void*)0); GL_ERROR();
+	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_SHORT, (void*)0); GL_ERROR();
 	//glDrawRangeElements(GL_TRIANGLES, 0, 3, 3, GL_UNSIGNED_SHORT, (void*)0);
 
 	//glBindBufferARB(GL_ELEMENT_ARRAY_BUFFER_ARB, indices_buffer_id);
